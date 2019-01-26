bb88b77a0c0052eeaf427e379ccb194820ef8317
10561544 <julio.villalva@autozone.com>
2019-01-25 18:53:32 -0700

    started landing styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,14 +3,19 @@ import {Link} from 'react-router-dom';
 
 
 const HomePage = ()=>(
-    <div className="page-container">        
-        <div className="content-container">
-            <div className="page-header">
+    <div className="page-container">              
+            <div className="landing">
                 <div className="content-container">
-                    <h1 className="page-header__title">main section</h1>                    
+                    <div className="landing__title">
+                        <span >June 8 - June 10, 2019</span>
+                        <img className="landing__logo" src="/images/autozone-color.svg" />
+                        <h1>Drive for Innovation</h1>
+                    </div>                                       
+                    <div className="landing__content">
+                        
+                    </div>                    
                 </div>
-            </div>  
-        </div>        
+            </div>                 
     </div>
 );
 
