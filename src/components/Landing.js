b6274d6194cae02531539d42d4184ec7d20b7242
10561544 <julio.villalva@autozone.com>
2019-01-31 09:16:18 -0700

    compress bg

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Landing = ()=> (
     <div className="landing">
-    <div className="content-container">
+    <div className="content-container landing__container">
         <div className="landing__title">
             <span >June 8 - June 10, 2019</span>
             <img className="landing__logo" src="/images/autozone-color.svg" />
